6082e74579e17b84b2e47b81518f8f79a4abb77c
Gerardo <gerardoalonso.martinezmonge@gmail.com>
2025-10-19 07:01:39 -0600

    Remove unused index parameter from projects.map in GridCards component

--- src/hero/pages/projects/card/GridCards.tsx
+++ src/hero/pages/projects/card/GridCards.tsx
@@ -5,7 +5,7 @@ import { Card } from "./Card"
 export const GridCards = ({ projects }: { projects: project[] }) => {
   return (
     <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {projects.map((project, index) => (
+      {projects.map((project) => (
         <Card key={project.title} project={project} />
       ))}
     </div>
